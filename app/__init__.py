ab1391da48aa313a230d98dd18a46b3e025956a7
nullbaka <uttaran.wary@gmail.com>
2020-08-18 05:15:18 +0530

    added talisman

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_talisman import Talisman
 
 
 app = Flask(__name__, static_url_path='', 
             static_folder='static')
+talisman = Talisman(app)
 socketio = SocketIO(app, engineio_logger=True)
 
 

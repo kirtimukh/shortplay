0c1ff8b8ca7a1be90692e0200db49da24113b06e
nullbaka <uttaran.wary@gmail.com>
2020-08-18 05:32:18 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,16 @@ from flask_socketio import SocketIO
 from flask_talisman import Talisman
 
 
+csp = {
+    'style-src': [
+            SELF,
+            'https://maxcdn.bootstrapcdn.com',
+        ],
+}
+
 app = Flask(__name__, static_url_path='', 
             static_folder='static')
-talisman = Talisman(app)
+talisman = Talisman(app, content_security_policy=csp)
 socketio = SocketIO(app, engineio_logger=True)
 
 

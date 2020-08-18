b9cf4c042d3280345707033bb473ac2752a95e7b
nullbaka <uttaran.wary@gmail.com>
2020-08-18 08:07:02 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,6 @@ csp = {
             '\'unsafe-inline\' https://cdnjs.cloudflare.com',
             '\'unsafe-inline\' https://ajax.googleapis.com'
         ],
-    'server': 'somesome'
 }
 
 app = Flask(__name__, static_url_path='', 

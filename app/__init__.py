b67b67f71a2bb29c15a681b0a03f04a93ae06154
nullbaka <uttaran.wary@gmail.com>
2020-08-18 06:32:09 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,26 +3,25 @@ from flask_socketio import SocketIO
 from flask_talisman import Talisman
 
 
-csp = {
-    'default-src': 'https://shortplay.herokuapp.com',
-    'style-src': [
-            'https://shortplay.herokuapp.com',
-            'https://maxcdn.bootstrapcdn.com',
-            'https://cdnjs.cloudflare.com',
-            'https://ajax.googleapis.com'
-        ],
-    'script-src': [
-            'https://shortplay.herokuapp.com',
-            'https://maxcdn.bootstrapcdn.com',
-            'https://cdnjs.cloudflare.com',
-            'https://ajax.googleapis.com'
-        ],
-}
+# csp = {
+#     'default-src': 'https://shortplay.herokuapp.com',
+#     'style-src': [
+#             'https://shortplay.herokuapp.com',
+#             'https://maxcdn.bootstrapcdn.com',
+#             'https://cdnjs.cloudflare.com',
+#             'https://ajax.googleapis.com'
+#         ],
+#     'script-src': [
+#             'https://shortplay.herokuapp.com',
+#             'https://maxcdn.bootstrapcdn.com',
+#             'https://cdnjs.cloudflare.com',
+#             'https://ajax.googleapis.com'
+#         ],
+# }
 
 app = Flask(__name__, static_url_path='', 
             static_folder='static')
-talisman = Talisman(app, content_security_policy=csp,
-                    content_security_policy_nonce_in=['script-src'])
+talisman = Talisman(app)
 socketio = SocketIO(app, engineio_logger=True)
 
 
